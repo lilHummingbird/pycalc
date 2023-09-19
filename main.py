6599e7c27dbdfabc01a59cb96fe87014427e4d42
Hummingbird <stphpedersen@gmail.com>
2023-09-19 09:57:53 +0200

    Complete!

---
+++ main.py
@@ -0,0 +1,53 @@
+# 
+# main.py
+#
+# Created by Persephone on 19.09.23 at 8.55
+#
+
+from mymath import add, subtract, multiply, divide, power, square
+
+def main():
+    print("Hello and welcome to pycalc!\nPlease choose from the list below what operation you'd like to do: \n")
+    print("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6.Square root\n7. Exit\n")
+    
+
+    match input():
+        case "1":
+            print(f"Type in the numbers you want to use:")
+            z = add(int(input()),int(input()))
+            print(f"Your result became {z}!\n")
+        case "2":
+            print(f"Type in the numbers you want to use:")
+            z = subtract(int(input()),int(input()))
+            print(f"Your result became {z}!\n")
+        case "3":
+            print(f"Type in the nymbers you want to use:")
+            z = multiply(int(input()),int(input()))
+            print(f"Your result became {z}!\n")
+        case "4":
+            print(f"Type in the numbers you want to use:")
+            z = divide(int(input()),int(input()))
+            print(f"Your result became {z}!\n")
+        case "5":
+            print(f"Type in the numbers you want to use:")
+            z = power(int(input()),int(input()))
+            print(f"Your result became {z}!\n")
+        case "6":
+            print(f"Type in the number(1) you want to use:")
+            z = square(int(input()))
+            print(f"Your result became {z}!\n")
+        case "7":
+            print(f"Thank you for using my calculator!")
+            return 0
+    
+          
+       
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ mymath.py
@@ -0,0 +1,28 @@
+#
+# math.py
+#
+# Created by Persephone on 19.09.23 at 09.27
+#
+from math import sqrt
+
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def power(x,y):
+    z = pow(x,y)
+    return z
+
+def square(x):
+    z = sqrt(x)
+    return z
+
